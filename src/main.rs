93fb5e7902c100e5d0b0da028869db3b6e3dc2a2
John Kugelman <john@kugelman.name>
2022-05-25 18:54:17 -0400

    Parse command-line arguments with `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    file_name: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
