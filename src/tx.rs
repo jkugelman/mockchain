3986759c7f3130f1b7b42e90412eedbca99bee9e
John Kugelman <john@kugelman.name>
2022-05-25 21:15:21 -0400

    Explain why currency values are stored as `Decimal`s

--- src/tx.rs
+++ src/tx.rs
@@ -12,6 +12,15 @@ pub type ClientId = u16;
 pub type TxId = u16;
 
 /// One entry from the transaction file.
+///
+/// Currency values are stored as `Decimal`s. Floating point numbers are a bad idea for currency due
+/// the errors introduced by their base-2 representation. A float can store `0.50` exactly but not
+/// `0.20`, for example.
+///
+/// Integers could be used if we stored cents instead of dollars. That would be awkward in this
+/// program, though, since the specification requires 4 decimal places of precision rather than 2.
+/// That also wouldn't translate well to other currencies. Not every currency is divisible into
+/// hundredths.
 #[derive(Clone, Debug)]
 pub enum Record {
     /// A deposit into a client's account.
